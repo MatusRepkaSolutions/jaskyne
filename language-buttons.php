a48aebee4b6952c5e5ae1e64d319bfb99434ce1f
Matúš Repka <162296215+MatusRepkaSolutions@users.noreply.github.com>
2026-03-29 15:25:57 +0200

    1

---
+++ language-buttons.php
@@ -0,0 +1,13 @@
+<div class="map-lang-switches">
+        <a href="#" class="map-lang-btn" data-lang-switch="svk">
+            <img src="img/index/slovakia.jpg" alt="Slovak">
+        </a>
+
+        <a href="#" class="map-lang-btn" data-lang-switch="hun">
+            <img src="img/index/hungary.png" alt="Hungarian">
+        </a>
+
+        <a href="#" class="map-lang-btn" data-lang-switch="eng">
+            <img src="img/index/england.png" alt="English">
+        </a>
+    </div>
--- mapa.php
+++ mapa.php
@@ -13,19 +13,7 @@
 
 <div class="map-page">
 
-    <div class="map-lang-switches">
-        <a href="#" class="map-lang-btn" data-lang-switch="svk">
-            <img src="img/index/slovakia.jpg" alt="Slovak">
-        </a>
-
-        <a href="#" class="map-lang-btn" data-lang-switch="hun">
-            <img src="img/index/hungary.png" alt="Hungarian">
-        </a>
-
-        <a href="#" class="map-lang-btn" data-lang-switch="eng">
-            <img src="img/index/england.png" alt="English">
-        </a>
-    </div>
+    <?php include language-buttons.php ?>
 
     <div class="map-title map-animate-title" data-txt="mapTitle">
         Jaskyne Slovenského a Aggteleckého krasu
