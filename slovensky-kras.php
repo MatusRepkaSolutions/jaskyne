1b83d34b0d6e633a6b7f5368ac82e54df353f60c
Matúš Repka <162296215+MatusRepkaSolutions@users.noreply.github.com>
2026-03-30 18:16:50 +0200

    1

--- slovensky-kras.php
+++ slovensky-kras.php
@@ -17,7 +17,7 @@
 
     <div class="global-control-buttons">
         <a class="home-button" href="mapa.php">
-            <img class="home-button-img" src="img/home-button.svg">
+            <img class="home-button-img" src="img/home-button.svg" alt="Domov">
         </a>
         <a href="aggtelek-kras.php" class="global-nav-btn" data-txt="btnAggtelekKarst">
             Jaskyne Aggteleckého krasu
@@ -33,35 +33,35 @@
     </div>
 
     <div class="kras-mapa">
-        <img src="img/kras-mapa/mapa.png" class="kras-mapa-img">
+        <img src="img/kras-mapa/mapa.png" class="kras-mapa-img" alt="Mapa jaskýň Slovenského krasu">
     </div>
 
-    <a href="#" class="map2-button pos1">
+    <a href="#" class="map2-button pos1" style="--i:0;">
         <div class="map2-button-marker"></div>
         <div class="map2-button-text" data-txt="ochotnicka">Ochtinská aragonitová jaskyňa</div>
     </a>
 
-    <a href="domnica.php" class="map2-button pos2">
+    <a href="domnica.php" class="map2-button pos2" style="--i:1;">
         <div class="map2-button-marker"></div>
         <div class="map2-button-text" data-txt="domica">Domica</div>
     </a>
 
-    <a href="#" class="map2-button pos3">
+    <a href="#" class="map2-button pos3" style="--i:2;">
         <div class="map2-button-text" data-txt="gombasecka">Gombasecká jaskyňa</div>
         <div class="map2-button-marker"></div>
     </a>
 
-    <a href="#" class="map2-button pos4">
+    <a href="#" class="map2-button pos4" style="--i:3;">
         <div class="map2-button-marker"></div>
         <div class="map2-button-text" data-txt="jasovska">Jasovská jaskyňa</div>
     </a>
 
-    <a href="#" class="map2-button pos5">
+    <a href="#" class="map2-button pos5" style="--i:4;">
         <div class="map2-button-marker"></div>
         <div class="map2-button-text" data-txt="krasnohorska">Krásnohorská jaskyňa</div>
     </a>
 
-    <a href="#" class="map2-button pos6">
+    <a href="#" class="map2-button pos6" style="--i:5;">
         <div class="map2-button-marker"></div>
         <div class="map2-button-text" data-txt="silicka">Silická ľadnica</div>
     </a>
