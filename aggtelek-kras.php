f59b805d82533161ae7e1cbe090c02fbc0d5adc0
Matúš Repka <162296215+MatusRepkaSolutions@users.noreply.github.com>
2026-04-19 15:54:44 +0200

    1

--- aggtelek-kras.php
+++ aggtelek-kras.php
@@ -32,32 +32,32 @@
         Jaskyne Aggteleckého krasu
     </div>
 
-    <a href="ochtinska.php" class="map2-button pos7" style="--i:0;" data-cave="baradla">
+    <a href="bradla.php" class="map2-button pos7" style="--i:0;" data-cave="baradla">
         <div class="map2-button-marker"></div>
         <div class="map2-button-text" data-txt="baradla">Jaskyňa Baradla</div>
     </a>
 
-    <a href="domnica.php" class="map2-button pos8" style="--i:1;" data-cave="imreho">
+    <a href="imreho.php" class="map2-button pos8" style="--i:1;" data-cave="imreho">
         <div class="map2-button-marker"></div>
         <div class="map2-button-text" data-txt="imreho">Jaskyňa Imreho Vassa</div>
     </a>
 
-    <a href="gombasecka.php" class="map2-button pos9" style="--i:2;" data-cave="beke">
-        <div class="map2-button-text special" data-txt="beke">Jaskyňa Béke</div>
+    <a href="beke.php" class="map2-button pos9" style="--i:2;" data-cave="beke">
         <div class="map2-button-marker"></div>
+        <div class="map2-button-text" data-txt="beke">Jaskyňa Béke</div>
     </a>
 
-    <a href="jasovska.php" class="map2-button pos10" style="--i:3;" data-cave="kossuthova">
+    <a href="kossuthova.php" class="map2-button pos10" style="--i:3;" data-cave="kossuthova">
         <div class="map2-button-marker"></div>
         <div class="map2-button-text" data-txt="kossuthova">Kossuthova jaskyňa</div>
     </a>
 
-    <a href="krasnohorska.php" class="map2-button pos11" style="--i:4;" data-cave="meteor">
+    <a href="meteor.php" class="map2-button pos11" style="--i:4;" data-cave="meteor">
         <div class="map2-button-marker"></div>
         <div class="map2-button-text" data-txt="meteor">Jaskyňa Meteor</div>
     </a>
 
-    <a href="silicka.php" class="map2-button pos12" style="--i:5;" data-cave="rakocziho">
+    <a href="rakocziho.php" class="map2-button pos12" style="--i:5;" data-cave="rakocziho">
         <div class="map2-button-marker"></div>
         <div class="map2-button-text" data-txt="rakocziho">Rákócziho jaskyňa</div>
     </a>
