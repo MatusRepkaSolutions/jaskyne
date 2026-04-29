e66142673579ae57d0fe8a8a99dd0fc3bbe8110e
Matúš Repka <162296215+MatusRepkaSolutions@users.noreply.github.com>
2026-04-29 11:16:43 +0200

    1

--- mapa.php
+++ mapa.php
@@ -15,7 +15,19 @@
 
     <!-- ######################## Upper controls ################## -->
 
-    <?php include 'language-buttons.php'; ?>
+    <div class="map-lang-switches">
+        <a href="#" class="map-lang-btn" data-lang-switch="svk">
+            <img src="img/index/slovakia.jpg" alt="Slovak">
+        </a>
+
+        <a href="#" class="map-lang-btn" data-lang-switch="hun">
+            <img src="img/index/hungary.png" alt="Hungarian">
+        </a>
+
+        <a href="#" class="map-lang-btn" data-lang-switch="eng">
+            <img src="img/index/england.png" alt="English">
+        </a>
+    </div>
 
     <!-- ########################################################## -->
 
--- slovensky-kras.php
+++ slovensky-kras.php
@@ -24,7 +24,19 @@
         </a>
     </div>
 
-    <?php include 'language-buttons.php'; ?>
+    <div class="map-lang-switches">
+        <a href="#" class="map-lang-btn" data-lang-switch="svk">
+            <img src="img/index/slovakia.jpg" alt="Slovak">
+        </a>
+
+        <a href="#" class="map-lang-btn" data-lang-switch="hun">
+            <img src="img/index/hungary.png" alt="Hungarian">
+        </a>
+
+        <a href="#" class="map-lang-btn" data-lang-switch="eng">
+            <img src="img/index/england.png" alt="English">
+        </a>
+    </div>
 
     <!-- ########################################################## -->
 
